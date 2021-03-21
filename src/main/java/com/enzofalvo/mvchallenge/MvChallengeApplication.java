f57b49712f0295f507ab7102a312eb303015597c
enzofalvo <enzo.fvo@gmail.com>
2021-03-21 15:48:06 -0300

    Conectando repositorio local com o github

---
+++ src/main/java/com/enzofalvo/mvchallenge/MvChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.enzofalvo.mvchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enzofalvo/mvchallenge/MvChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzofalvo.mvchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
